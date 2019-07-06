006e410463eda783f772ddf964fc997275ab62ab
Tai Henrichs <tai.henrichs@gmail.com>
2019-07-06 16:34:39 -0500

    Unicafe Step 5

--- Week1/unicafe/unicafe/src/index.js
+++ Week1/unicafe/unicafe/src/index.js
@@ -1,19 +1,16 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+//Label Variables
+const g = 'Good'
+const n = 'Neutral'
+const b = 'Bad'
+
 const App = () => {
   // save clicks of each button to own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
-  //Label Variables
-  const g = 'Good'
-  const n = 'Neutral'
-  const b = 'Bad'
-
-  let total = sum(good, neutral, bad)
-  let avg = sum(good, bad * -1) / total
   
   return (
     <div>
@@ -21,14 +18,44 @@ const App = () => {
       <Button onClick = {() => setGood(good + 1)} text = {g}/>
       <Button onClick = {() => setNeutral(neutral + 1)} text = {n}/>
       <Button onClick = {() => setBad(bad + 1)} text = {b}/>
+      
+      <Statistics good = {good} bad = {bad} neutral = {neutral}/>
+    </div>
+    
+  )
+}
 
+const Statistics = (props) => {
+  let total = sum(props.good, props.neutral, props.bad)
+  let avg = sum(props.good, props.bad * -1) / total
+
+  if (total <= 0) {
+    return (
+      <div>
+        <Display disp = 'Statistics'/>
+        <Display disp = 'No Feedback Given'/>
+      </div>
+    )
+  }
+  return (
+    <div>
       <Display disp = 'Statistics'/>
-      <Display disp = {g} msg = {good}/>
-      <Display disp = {n} msg = {neutral}/>
-      <Display disp = {b} msg = {bad}/>
-      <Display disp = "Total Reviews" msg = {total}/>
-      <Display disp = "Average" msg = {avg}/>
-      <Display disp = "Percent Positive" msg = {good / total}/>
+      <Statistic text = {g} value = {props.good}/>
+      <Statistic text = {n} value = {props.neutral}/>
+      <Statistic text = {b} value = {props.bad}/>
+      <Statistic text = "Total Reviews:" value = {total}/>
+      <Statistic text = "Average:" value = {avg}/>
+      <Statistic text = "Percent Positive:" value = {props.good / total}/>
+    </div>
+  )
+}
+//To the grader: Sorry!  I had already been using the Display component located
+//further below.  I'm aware this Statistic implementation is practically pointless
+//since it is functionally identical to the Display component.
+const Statistic = ({text, value}) => {
+  return (
+    <div>
+      <Display disp = {text} msg = {value}/>
     </div>
   )
 }
@@ -41,7 +68,6 @@ const sum = (...vals) => {
   return sum
 }
 
-//Rendering Components
 const Button = ({onClick, text}) => {
     return (
         <button onClick = {onClick}>
